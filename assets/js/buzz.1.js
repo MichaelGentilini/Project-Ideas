f4dd2a4f1b557764c0a61aba932a5e00bea53de3
dallasappraiser <46821219+dallasappraiser@users.noreply.github.com>
2019-04-21 17:53:03 -0500

    newer 1 and 2 versions

--- assets/js/buzz.1.js
+++ assets/js/buzz.1.js
@@ -1,5 +1,14 @@
-// ! going to use this to test various icons
+// ? this can be used to display different icons by category
+var catArray = [];
 
+// ! Various icons for different Establishments
+var standardIcon = "https://img.icons8.com/windows/32/000000/coconut-cocktail.png"; //srestaurant
+var beerIcon = "https://img.icons8.com/color/32/000000/beer.png";
+var wineIcon = "https://img.icons8.com/color/32/000000/wine-glass.png";
+var whiskeyIcon = "https://img.icons8.com/color/32/000000/sport.png";
+var pubIcon = "https://img.icons8.com/dusk/32/000000/beer.png";
+var newAmericanIcon = "https://img.icons8.com/color/48/000000/usa.png";
+var cocktailIcon = "https://img.icons8.com/windows/32/000000/cocktail.png";
 
 
 // ? This is for the GOOGLE Map API ??????
@@ -11,11 +20,16 @@ function initMap() {
 
   var infoPopUp = new google.maps.InfoWindow;
   var marker, i;
+  // catArray = catArray;
 
   for (i = 0; i < searchLimit; i++) {
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(latArray[i], lngArray[i]),
-      icon: "https://img.icons8.com/color/48/000000/wine-glass.png",
+      // if (catArray[0] == "newamerican") {
+      //   icon: newAmericanIcon,
+      // }
+      // else {
+      icon: standardIcon,
       map: map
     });
 
@@ -34,7 +48,7 @@ var key =
   "JARq9NBksYNIfR1HQQ8z3P5r7ypZW9-Xo_bVQUO-QRgXM3XJbnpvhKuo25EXjDrm1Xq8A9Vv6-p9dHcRJlH6dVqQVbXLU_iq3CYqI1YVwxyD12qLi0-xDNo8_ba5XHYx";
 
 var term = "term=happy%20hour";
-var businessID = "VJyE0wCtZtoLev9YgXYpIQ";
+var businessID = "";
 
 // ! Empty variables for storing information to pass between API's
 var businessName = "";
@@ -47,8 +61,7 @@ var lngArray = [];
 // ? will need this for local storage
 var idArray = [];
 
-// ? this can be used to display different icons by category
-var catArray = [];
+
 
 // ? this works when set to null to start
 var userloc = null;
@@ -64,6 +77,9 @@ function reset() {
   latArray = [];
   lngArray = [];
   catArray = [];
+
+  // ! added
+  $('.dynamic').css("visibility", "hidden");
 }
 // ! on click funtion for reset
 $('#reset-search').on("click", function () {
@@ -139,15 +155,91 @@ $('#submit-search').on("click", function (event) {
             var cats = [data.businesses[i].categories[0].alias];
             catArray.push(cats);
             catArray = catArray;
-
             initMap();
           }
-          console.log(catArray);
+          $(".bar-btn").on("click", function () {
+            var newBusinessID = ($(this).attr('bar-code'));
+            businessID = newBusinessID
+
+            // todo This URL is used to query the API for a particular bar
+            var buzzDetailURL =
+              "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" +
+              businessID;
+
+            // todo This URL is used to query the API for the reviews of a particular bar
+            var buzzReviewURL =
+              "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" +
+              businessID +
+              "/reviews";
+
+            function getDataByID() {
+              $.ajax({
+                url: buzzDetailURL,
+                type: "GET",
+                beforeSend: function (xhr) {
+                  xhr.setRequestHeader("Authorization", "Bearer " + key);
+                  xhr.setRequestHeader("X-Requested-With", "true");
+                },
+                success: function (data) {
+                  $('.info,.image-here,.reviews').empty();
+                  console.log(data);
+                  $('.dynamic').css("visibility", "visible");
+                  var name = $("<h2>").html(data.name);
+                  name.addClass("bar-name");
+                  var price = $("<h4>").html("Price: " + data.price);
+                  var rating = $("<h4>").html("Rating: " + data.rating);
+                  var cat = $("<h4>").html("Category: " + data.categories[0].title);
+                  var addr = $("<h4>").html("Street address: " + data.location.display_address);
+                  var phone = $("<h4>").html("Phone: " + data.display_phone);
+                  $(".info").append(name, price, rating, cat, addr, phone);
+
+                  // ? adding photos
+                  for (let j = 0; j < data.photos.length; j++) {
+                    var photos = $("<img>");
+                    photos.attr("src", data.photos[j]);
+                    photos.css("height", "200px");
+                    $(".image-here").append(photos);
+                  }
+                  // ? adding revews  (last call)
+                  getreviewsByID();
+                }
+              });
+            }
+
+            function getreviewsByID() {
+              $.ajax({
+                url: buzzReviewURL,
+                type: "GET",
+                beforeSend: function (xhr) {
+                  xhr.setRequestHeader("Authorization", "Bearer " + key);
+                  xhr.setRequestHeader("X-Requested-With", "true");
+                },
+
+                success: function (data) {
+                  var reviewTitle = $("<h2>").html("Photos and Reviews");
+                  reviewTitle.css('text-align', 'center');
+                  $(".info").append(reviewTitle);
+                  for (let k = 0; k < data.reviews.length; k++) {
+                    var reviews = $("<h5>").html(data.reviews[k].text);
+                    reviews.addClass('user-reviews')
+                    $(".reviews").append(reviews);
+                  }
+                }
+              });
+            }
+
+            getDataByID();
+          })
         }
         barButton();
       }
     });
   }
+
+
+
+
+
   getData();
 })
 
